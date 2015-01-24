924666208a2d6f1f330c378307cb4be411428790
Kevin M. Smith <kevin.smith@tufts.edu>
2015-01-24 18:06:14 -0500

    stop conflict on new tables

--- R/bootstrap-swift.R
+++ R/bootstrap-swift.R
@@ -4,13 +4,33 @@ BootstrapSwift <- function(config) {
   
   sites <- .GetAllSites(conn = conn, config = config)
   
- startDate <- lubridate::floor_date(now(tz="utc") + .ParseISO8601Offset("-06:00") - .ParseISO8601Duration("P5W"), "day")
- endDate   <- lubridate::floor_date(now(tz="utc") + .ParseISO8601Offset("-06:00") - .ParseISO8601Duration("P1D"), "day")
+ startDate <- lubridate::floor_date(lubridate::now(tz="utc") + .ParseISO8601Offset("-06:00") - .ParseISO8601Duration("P5W"), "day")
+ endDate   <- lubridate::floor_date(lubridate::now(tz="utc") + .ParseISO8601Offset("-06:00") - .ParseISO8601Duration("P1D"), "day")
  dates     <- strftime(seq(startDate, endDate, by = "day"), '%Y-%m-%d')
  
   pb <- txtProgressBar(min = 1, max = nrow(sites), style = 3, width = 20)
   
-  cc <- foreach(i = 1:nrow(sites)) %dopar% {
+ 
+ cc <- foreach(i = 1) %dopar% {
+   setTxtProgressBar(pb, i)
+   
+   DownloadWrapper <- function(date) {
+     
+     .DownloadDataFromNWIS(site = sites[i,1],
+                           params = config$collections$params,
+                           startDate = date,
+                           endDate = date,
+                           period = NULL,
+                           offset = config$time$midnight.offset.standard, 
+                           stage = TRUE,
+                           config = config)
+     
+   }
+   
+   lapply(dates, DownloadWrapper)
+ } 
+ 
+  cc <- foreach(i = 2:nrow(sites)) %dopar% {
        setTxtProgressBar(pb, i)
        
        DownloadWrapper <- function(date) {
