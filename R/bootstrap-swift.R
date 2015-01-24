64586a26c0d067bf90ae26a00c90a2e3785a9d42
Kevin M. Smith <kevin.smith@tufts.edu>
2015-01-24 22:04:03 +0000

    debug

--- R/bootstrap-swift.R
+++ R/bootstrap-swift.R
@@ -7,7 +7,9 @@ BootstrapSwift <- function(config) {
 #  startDate <- lubridate::floor_date(now(tz="utc") + .ParseISO8601Offset("-06:00") - .ParseISO8601Duration("P5W"), "day")
 #  endDate   <- lubridate::floor_date(now(tz="utc") + .ParseISO8601Offset("-06:00") - .ParseISO8601Duration("P1D"), "day")
 #  dates     <- strftime(seq(startDate, endDate, by = "day"), '%Y-%m-%d')
-  
+ 
+dates <- c("a", "b")
+
   pb <- txtProgressBar(min = 1, max = nrow(sites) * length(dates), style = 3, width = 20)
   
   cc <-  foreach(j = 1:length(dates)) %:% foreach(i = 1:nrow(sites)) %dopar% {
