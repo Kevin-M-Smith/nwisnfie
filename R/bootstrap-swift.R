7c56929d01068a15311858bfde0b89683c2e4731
Kevin M. Smith <kevin.smith@tufts.edu>
2015-01-25 19:41:52 -0500

    test

--- R/bootstrap-swift.R
+++ R/bootstrap-swift.R
@@ -8,51 +8,51 @@ BootstrapSwift <- function(config) {
   endDate   <- lubridate::floor_date(lubridate::now(tz="utc") + .ParseISO8601Offset("-06:00") - .ParseISO8601Duration("P1D"), "day")
   dates     <- strftime(seq(startDate, endDate, by = "day"), '%Y-%m-%d')
   
-  pb <- txtProgressBar(min = 1, max = nrow(sites), style = 3, width = 20)
+#  pb <- txtProgressBar(min = 1, max = nrow(sites), style = 3, width = 20)
   
-  cc <- foreach(i = 1) %dopar% {
-    setTxtProgressBar(pb, i)
-    
-    DownloadWrapper <- function(date) {
-      
-      .DownloadDataFromNWIS(site = sites[i,1],
-                            params = config$collections$params,
-                            startDate = date,
-                            endDate = date,
-                            period = NULL,
-                            offset = config$time$utc.offset, 
-                            stage = TRUE,
-                            config = config)
-      
-      table <- paste0(config$tables$staging, date)
-            
-      DisableAutovacuum(table = table, config = config)
-      
-    }
-    
-    lapply(dates, DownloadWrapper)
-  } 
-  
-  #cc <- foreach(i = 2:nrow(sites)) %dopar% {
-  cc <- foreach(i = 2:10) %dopar% { 
-    setTxtProgressBar(pb, i)
-    
-    DownloadWrapper <- function(date) {
-      
-      .DownloadDataFromNWIS(site = sites[i,1],
-                            params = config$collections$params,
-                            startDate = date,
-                            endDate = date,
-                            period = NULL,
-                            offset = config$time$utc.offset, 
-                            stage = TRUE,
-                            config = config)
-      
-    }
-    
-    lapply(dates, DownloadWrapper)
-  }
-  
+#   cc <- foreach(i = 1) %dopar% {
+#     setTxtProgressBar(pb, i)
+#     
+#     DownloadWrapper <- function(date) {
+#       
+#       .DownloadDataFromNWIS(site = sites[i,1],
+#                             params = config$collections$params,
+#                             startDate = date,
+#                             endDate = date,
+#                             period = NULL,
+#                             offset = config$time$utc.offset, 
+#                             stage = TRUE,
+#                             config = config)
+#       
+#       table <- paste0(config$tables$staging, date)
+#             
+#       DisableAutovacuum(table = table, config = config)
+#       
+#     }
+#     
+#     lapply(dates, DownloadWrapper)
+#   } 
+#   
+#   #cc <- foreach(i = 2:nrow(sites)) %dopar% {
+#   cc <- foreach(i = 2:10) %dopar% { 
+#     setTxtProgressBar(pb, i)
+#     
+#     DownloadWrapper <- function(date) {
+#       
+#       .DownloadDataFromNWIS(site = sites[i,1],
+#                             params = config$collections$params,
+#                             startDate = date,
+#                             endDate = date,
+#                             period = NULL,
+#                             offset = config$time$utc.offset, 
+#                             stage = TRUE,
+#                             config = config)
+#       
+#     }
+#     
+#     lapply(dates, DownloadWrapper)
+#   }
+#   
   
   stagingTable <- "staging_2014-12-20"
   
